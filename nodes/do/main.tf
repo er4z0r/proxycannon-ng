de7c378d4efb83dc8cae3eaa4c1380ffea742701
er4z0r <mail@er4z0r.com>
2020-04-09 14:23:47 +0200

    change to assign the resources to existing project previously a new 'proxycannon-ng' project was created and later destroyed.

--- nodes/do/main.tf
+++ nodes/do/main.tf
@@ -4,35 +4,36 @@ provider "digitalocean" {
 
 
 data "digitalocean_ssh_key" "pentest" {
-  name = element(var.ssh_keys, count.index)
+  name  = element(var.ssh_keys, count.index)
   count = length(var.ssh_keys)
 }
 
-#create new project for every client
-resource "digitalocean_project" "proxycannon-ng" {
-  name        = "proxycannon-ng"
-  description = "Project to extend the proxycannon-ng project to use DO"
-  purpose =   "Class project / Educational purposes"
-  resources = digitalocean_droplet.exit_node.*.urn
+data "digitalocean_project" "proxycannon_project" {
+  name = "proxycannon-ng"
 }
 
 resource "digitalocean_droplet" "vpn_server" {
-  image  = "ubuntu-18-04-x64"
-  name   = "vpn-server"
-  region = var.do_region
-  size   = var.do_droplet_size
-  ssh_keys = data.digitalocean_ssh_key.pentest.*.fingerprint
+  image              = "ubuntu-18-04-x64"
+  name               = "vpn-server"
+  region             = var.do_region
+  size               = var.do_droplet_size
+  ssh_keys           = data.digitalocean_ssh_key.pentest.*.fingerprint
   private_networking = true
-  tags = ["vpn-server"]
+  tags               = ["vpn-server"]
 }
 
 resource "digitalocean_droplet" "exit_node" {
-  image  = "ubuntu-18-04-x64"
-  name   = "exit-node-${count.index+1}"
-  region = var.do_region
-  size   = var.do_droplet_size
-  ssh_keys = data.digitalocean_ssh_key.pentest.*.fingerprint
-  count = var.node_count
+  image              = "ubuntu-18-04-x64"
+  name               = "exit-node-${count.index + 1}"
+  region             = var.do_region
+  size               = var.do_droplet_size
+  ssh_keys           = data.digitalocean_ssh_key.pentest.*.fingerprint
+  count              = var.node_count
   private_networking = true
-  tags = ["exit-node"]
+  tags               = ["exit-node"]
+}
+
+resource "digitalocean_project_resources" "proxycannon_resources" {
+  project   = data.digitalocean_project.proxycannon_project.id
+  resources = concat(digitalocean_droplet.exit_node.*.urn, [digitalocean_droplet.vpn_server.urn])
 }
