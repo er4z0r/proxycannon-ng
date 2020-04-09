bd9ce8496574b523c7bf0db12e5599d0e46a5efc
er4z0r <mail@er4z0r.com>
2020-04-09 16:00:03 +0200

    added exit-node setup via remote-exec provisioner

--- nodes/do/main.tf
+++ nodes/do/main.tf
@@ -31,6 +31,26 @@ resource "digitalocean_droplet" "exit_node" {
   count              = var.node_count
   private_networking = true
   tags               = ["exit-node"]
+
+  connection {
+      host     = self.ipv4_address
+      type     = "ssh"
+      user     = "root"
+      private_key = file("${var.private_key}")
+  }
+
+
+  # upload our provisioning scripts
+  provisioner "file" {
+    source      = "${path.module}/configs/"
+    destination = "/tmp/"
+  }
+
+  # execute our provisioning scripts
+  provisioner "remote-exec" {
+    script = "${path.module}/configs/node_setup.bash"
+  }
+
 }
 
 resource "digitalocean_project_resources" "proxycannon_resources" {
