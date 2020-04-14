32d9b9a43afbc0036426d2c5968096b4ac9af03f
er4z0r <mail@er4z0r.com>
2020-04-14 15:21:22 +0200

    attempt at setting up the control-server using TF provisioners are definitely a crutch and must be replaced by ansible

--- nodes/do/main.tf
+++ nodes/do/main.tf
@@ -14,46 +14,89 @@ data "digitalocean_project" "proxycannon_project" {
 
 resource "digitalocean_droplet" "vpn_server" {
   image              = "ubuntu-18-04-x64"
-  name               = "vpn-server"
+  name               = "proxycannon-server"
   region             = var.do_region
   size               = var.do_droplet_size
   ssh_keys           = data.digitalocean_ssh_key.pentest.*.fingerprint
   private_networking = true
-  tags               = ["vpn-server"]
-}
-
-resource "digitalocean_droplet" "exit_node" {
-  image              = "ubuntu-18-04-x64"
-  name               = "exit-node-${count.index + 1}"
-  region             = var.do_region
-  size               = var.do_droplet_size
-  ssh_keys           = data.digitalocean_ssh_key.pentest.*.fingerprint
-  count              = var.node_count
-  private_networking = true
-  tags               = ["exit-node"]
+  tags               = ["proxycannon-server"]
 
   connection {
       host     = self.ipv4_address
       type     = "ssh"
       user     = "root"
       private_key = file("${var.private_key}")
-  }
+  } 
 
-
-  # upload our provisioning scripts
+  # upload the config files
   provisioner "file" {
-    source      = "${path.module}/configs/"
+    source      = "${path.module}/configs/control_server"
     destination = "/tmp/"
   }
 
+  # upload the config files
+  provisioner "file" {
+    source      = "${path.module}/scripts/control_server"
+    destination = "/tmp/"
+  }
+
+
   # execute our provisioning scripts
   provisioner "remote-exec" {
-    script = "${path.module}/configs/node_setup.bash"
+    inline = ["cd /tmp/control_server", "sh install.sh"]
+  } 
+
+  # download keys for vpn server using an INSECURE scp command
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.private_key} root@${self.ipv4_address}:~/client-config.tar.gz . && tar xvzf client-config.tar.gz "
+  }
+
+  # copy the vpn config template  
+  provisioner "local-exec" {
+    command = "cp ${path.module}/configs/client/proxycannon-client.conf client-config/"
+  }
+
+  # modify client config with remote IP of this server
+  provisioner "local-exec" {
+    command = "sed -i 's/REMOTE_PUB_IP/${self.ipv4_address}/' proxycannon-client.conf"
+    working_dir = "client-config"
   }
 
 }
 
+# resource "digitalocean_droplet" "exit_node" {
+#   image              = "ubuntu-18-04-x64"
+#   name               = "exit-node-${count.index + 1}"
+#   region             = var.do_region
+#   size               = var.do_droplet_size
+#   ssh_keys           = data.digitalocean_ssh_key.pentest.*.fingerprint
+#   count              = var.node_count
+#   private_networking = true
+#   tags               = ["exit-node"]
+
+#   connection {
+#       host     = self.ipv4_address
+#       type     = "ssh"
+#       user     = "root"
+#       private_key = file("${var.private_key}")
+#   }
+
+
+#   # upload our provisioning scripts
+#   provisioner "file" {
+#     source      = "${path.module}/configs/exit_node"
+#     destination = "/tmp/"
+#   }
+
+#   # execute our provisioning scripts
+#   provisioner "remote-exec" {
+#     script = "${path.module}/configs/exit_node/node_setup.bash"
+#   }
+
+# }
+
 resource "digitalocean_project_resources" "proxycannon_resources" {
   project   = data.digitalocean_project.proxycannon_project.id
-  resources = concat(digitalocean_droplet.exit_node.*.urn, [digitalocean_droplet.vpn_server.urn])
+  # resources = concat(digitalocean_droplet.exit_node.*.urn, [digitalocean_droplet.vpn_server.urn])
+  resources = [digitalocean_droplet.vpn_server.urn]
 }
---
+++ nodes/do/scripts/control_server/install.sh
@@ -0,0 +1,71 @@
+#! /bin/sh
+# proxycannon-ng
+#
+
+###################
+# install software
+###################
+# update and install deps
+apt-get update --quiet
+apt-get install -y  --quiet openvpn easy-rsa
+
+
+################
+# setup openvpn
+################
+# cp configs
+cp configs/node-server.conf /etc/openvpn/node-server.conf
+cp configs/client-server.conf /etc/openvpn/client-server.conf
+
+# setup ca and certs
+mkdir /etc/openvpn/ccd
+make-cadir /etc/openvpn/easy-rsa
+cd /etc/openvpn/easy-rsa/
+ln -s openssl-1.0.0.cnf openssl.cnf
+mkdir keys
+. /etc/openvpn/easy-rsa/vars
+./clean-all
+/etc/openvpn/easy-rsa/pkitool --initca
+/etc/openvpn/easy-rsa/pkitool --server server
+/usr/bin/openssl dhparam -out /etc/openvpn/easy-rsa/keys/dh2048.pem 2048
+openvpn --genkey --secret /etc/openvpn/easy-rsa/keys/ta.key
+
+# generate certs
+for x in $(seq -f "%02g" 1 10);do /etc/openvpn/easy-rsa/pkitool client$x;done
+/etc/openvpn/easy-rsa/pkitool node01
+
+# start services
+systemctl start openvpn@node-server.service
+systemctl start openvpn@client-server.service
+
+###################
+# setup networking
+###################
+# setup routing and forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# use L4 (src ip, src dport, dest ip, dport) hashing for load balancing instead of L3 (src ip ,dst ip)
+#echo 1 > /proc/sys/net/ipv4/fib_multipath_hash_policy
+sysctl -w net.ipv4.fib_multipath_hash_policy=1
+
+# setup a second routing table
+echo "50        loadb" >> /etc/iproute2/rt_tables
+
+# set rule for openvpn client source network to use the second routing table
+ip rule add from 10.10.10.0/24 table loadb
+
+# always snat from eth0
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+############################
+# post install instructions
+############################
+
+mkdir ~/client-config
+cp /etc/openvpn/easy-rsa/keys/ta.key ~/client-config
+cp /etc/openvpn/easy-rsa/keys/ca.crt ~/client-config
+cp /etc/openvpn/easy-rsa/keys/client01.crt ~/client-config
+cp /etc/openvpn/easy-rsa/keys/client01.key ~/client-config
+
+cd
+tar cvzf client-config.tar.gz client-config
